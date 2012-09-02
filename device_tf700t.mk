58cb992db135fd6f0ab9795d00c9ca0ddec2b2ac
nycbjr <nycbjr@gmail.com>
2012-09-02 10:58:40 -0400

    forgot to revert prebuilt kernel fully

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -22,11 +22,11 @@ $(call inherit-product-if-exists, vendor/asus/tf700t/tf700t-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/asus/tf700t/overlay
 
 # Prebuilt kernel location
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-        LOCAL_KERNEL := device/asus/tf700t/kernel
-else
-        LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#        LOCAL_KERNEL := device/asus/tf700t/kernel
+#else
+#        LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
 
 # Files needed for boot image
 PRODUCT_COPY_FILES += \
