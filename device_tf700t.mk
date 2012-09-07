755e6c8b14a271e691ac5930082a2372ac3adb62
nycbjr <nycbjr@gmail.com>
2012-09-07 18:52:10 -0400

    back to inline

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -22,15 +22,14 @@ $(call inherit-product-if-exists, vendor/asus/tf700t/tf700t-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/asus/tf700t/overlay
 
 # Prebuilt kernel location
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-        LOCAL_KERNEL := device/asus/tf700t/kernel
-else
-        LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#        LOCAL_KERNEL := device/asus/tf700t/kernel
+#else
+#        LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
 
 # Files needed for boot image
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel \
     $(LOCAL_PATH)/ramdisk/init.cardhu.rc:root/init.cardhu.rc \
     $(LOCAL_PATH)/ramdisk/init.cardhu.keyboard.rc:root/init.cardhu.keyboard.rc \
     $(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
@@ -38,9 +37,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/init.cardhu.usb.rc:root/init.cardhu.usb.rc \
     $(LOCAL_PATH)/ramdisk/init.cardhu.cpu.rc:root/init.cardhu.cpu.rc
 
+    #$(LOCAL_KERNEL):kernel \
+
 # Kernel modules
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/lib/baseband_usb_chr.ko:system/lib/modules/baseband_usb_chr.ko \
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/prebuilt/lib/baseband_usb_chr.ko:system/lib/modules/baseband_usb_chr.ko \
     $(LOCAL_PATH)/prebuilt/lib/baseband-xmm-power2.ko:system/lib/modules/baseband-xmm-power2.ko \
     $(LOCAL_PATH)/prebuilt/lib/raw_ip_net.ko:system/lib/modules/raw_ip_net.ko \
     $(LOCAL_PATH)/prebuilt/lib/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
