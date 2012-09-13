c03acad01ba6bf78faeaeff9a687923fde70d2a4
Brett Richter <brichter@mintypro.(none)>
2012-09-13 06:14:26 -0400

    forgot to make it mdpi

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -21,6 +21,11 @@ $(call inherit-product-if-exists, vendor/asus/tf700t/tf700t-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/tf700t/overlay
 
+# This device is hdpi.
+PRODUCT_AAPT_CONFIG := normal hdpi mdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_LOCALES += hdpi
+
 # Prebuilt kernel location
 #ifeq ($(TARGET_PREBUILT_KERNEL),)
 #        LOCAL_KERNEL := device/asus/tf700t/kernel
