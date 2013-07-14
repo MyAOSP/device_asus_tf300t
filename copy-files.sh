c239055329d7b12c17a1932682eff5a77303fb74
drgravy <drg113001@gmail.com>
2013-07-14 07:45:07 -0500

    fix scripts

--- copy-files.sh
+++ copy-files.sh
@@ -8,27 +8,13 @@ fi
 BASE=../../../vendor/asus/tf300t/proprietary
 rm -rf $BASE/*
 
-FILES=`cat proprietary-files.txt`
-for FILE in $FILES; do
+for FILE in `cat proprietary-files.txt`; do
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
     fi
     cp $SYSDIR/$FILE $BASE/$FILE
 done
-
-# Check MD5 sums of proprietary files
-(cd $BASE; md5sum $FILES) | \
-    diff -u proprietary-files.md5sum -
-if [ $? != 0 ]; then
-    echo
-    echo "MD5 mismatch on proprietary files."
-    echo "Your build might not work with these files."
-    echo
-    echo "If you know, what you are doing, please run ./setup-makefiles.sh manually."
-    echo
-    exit 1
-fi
 
 ./setup-makefiles.sh
 
--- extract-files.sh
+++ extract-files.sh
@@ -3,8 +3,7 @@
 BASE=../../../vendor/asus/tf300t/proprietary
 rm -rf $BASE/*
 
-FILES=`cat proprietary-files.txt`
-for FILE in $FILES; do
+for FILE in `cat proprietary-files.txt`; do
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
@@ -12,18 +11,6 @@ for FILE in $FILES; do
     adb pull /system/$FILE $BASE/$FILE
 done
 
-# Check MD5 sums of proprietary files
-#(cd $BASE; md5sum $FILES) | \
-#    diff -u proprietary-files.md5sum -
-#if [ $? != 0 ]; then
-#    echo
-#    echo "MD5 mismatch on proprietary files."
-#    echo "Your build might not work with these files."
-#    echo
-#    echo "If you know, what you are doing, please run ./setup-makefiles.sh manually."
-#    echo
-#    exit 1
-#fi
-
 ./setup-makefiles.sh
+
 
